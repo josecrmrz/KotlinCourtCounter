179c124f6d965d93ac9b498ff36593e8b99ed907
Jose Ramirez <josecrmrz@gmail.com>
2018-02-02 23:37:31 -0600

    Add some button fuctionality

--- app/src/main/java/com/example/android/kotlincourtcounter/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlincourtcounter/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.android.kotlincourtcounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        three_points.setOnClickListener {
+            threePoints()
+        }
+
+        two_points.setOnClickListener {
+            twoPoints()
+        }
+
+        free_throw.setOnClickListener {
+            freeThrow()
+        }
+
+        displayForTeamA(6)
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    private fun displayForTeamA(score: Int) {
+        team_a_score.text = score.toString()
+    }
+
+    fun threePoints() {
+        team_a_score.text = 3.toString()
+    }
+
+    fun twoPoints() {
+        team_a_score.text = 2.toString()
+    }
+
+    fun freeThrow() {
+        team_a_score.text = 1.toString()
     }
 }
